666644f727ed22a5dd3f3ae682b1ee08133be56c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2019-05-03 12:13:50 -0500

    Use the brew-installed version of bash

--- brew.sh
+++ brew.sh
@@ -5,15 +5,26 @@ if ! command -v brew > /dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Make sure we're using the latest Homebrew
+# Make sure we're using the latest homebrew
 brew update
 
 # Upgrade any already-installed formulae
 brew upgrade
 
-# Dev tools
+# Save homebrew’s install location
+BREW_PREFIX=$(brew --prefix)
+
+# Install latest bash
 brew install bash
 brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! grep -F -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Dev tools
 brew install git
 
 # Remove outdated versions from the cellar
