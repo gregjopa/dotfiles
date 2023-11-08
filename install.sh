8daa977d7a98be82ffed0f39bbf1b744823e2c9c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-11-08 14:21:03 -0600

    Update oh-my-zsh install link

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ if [ -d $HOME/.oh-my-zsh ]; then
   echo "Oh-My-Zsh is already installed"
 else
   echo "Installing Oh-My-Zsh"
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 if command -v volta &> /dev/null; then
