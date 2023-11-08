f4925c23badde5a4b839bc66806a9a4da14fe9f7
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-11-08 14:20:42 -0600

    Update homebrew install link

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if command -v brew &> /dev/null; then
   echo "Homebrew is already installed"
 else
   echo "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Make sure we're using the latest homebrew
